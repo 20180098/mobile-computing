b1b9cfdc97bf1be45966c6097d27aff10548f46e
20180098 <20180098@students.htl-perg.ac.at>
2021-11-18 13:27:40 +0100

    initial commit

---
+++ NumberGuess/NumberGuess/NumberGuessModel.swift
@@ -0,0 +1,37 @@
+import Foundation
+import UIKit
+
+class NumberGuessModel {
+    
+    var target = 0
+    var guessCount = 0
+    
+    func compare(to: Int) -> Int {
+        return target - to
+    }
+    
+    func isValid(string: String?) -> Bool {
+        var isValid = false
+        if let str = string, let guess = Int(str) {
+            if guess >= 0 && guess <= 100 {
+                isValid = true
+            }
+        }
+        return isValid
+    }
+    
+    func getImage() -> String {
+        var picture: String = ""
+        
+        if (guessCount < 6) {
+            picture = "happy.png"
+        } else if (guessCount < 10) {
+            picture = "neutral.png"
+        } else {
+            picture = "sad.png"
+        }
+        return picture
+    }
+    
+}
+
--- NumberGuess/NumberGuess/ViewController.swift
+++ NumberGuess/NumberGuess/ViewController.swift
@@ -1,19 +1,54 @@
-//
+///
 //  ViewController.swift
-//  NumberGuess
+//  NumberGuesser_2
 //
-//  Created by Nico Rausch on 18.11.21.
+//  Created by Nico Rausch on 07.10.21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var EdtGuess: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var btnGuess: UIButton!
+    var model = NumberGuessModel()
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        print(model.target)
+        model.guessCount += 1
+        if model.isValid(string: EdtGuess.text) {
+            let compare = model.compare(to: Int(EdtGuess.text!)!)
+            print(compare)
+            if (compare > 0) {
+                label.text = "Zu niedrig!"
+            } else if (compare < 0) {
+                label.text = "Zu hoch"
+            } else {
+                label.text = ("Du hast \(model.guessCount) Versuche gebraucht")
+                let imgName: String = model.getImage()
+                image.image = UIImage(named: imgName)
+                image.isHidden = false
+            }
+        }
+    }
+    
+    @IBAction func onEdtTextChanged(_ sender: UITextField) {
+        print("text changed: \(sender.text)")
+        btnGuess.isEnabled = model.isValid(string: sender.text)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        model.target = Int.random(in: 0...100)
+        print(model.target)
+        label.text = ""
+        label.textAlignment = NSTextAlignment.center
+        btnGuess.setTitle("Guess", for: .normal)
     }
-
-
+    
+    
 }
-
