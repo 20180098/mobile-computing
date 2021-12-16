9fc5e4d5f7e058c580d14bd1d4e5c4fc9ae002d3
20180098 <94611195+20180098@users.noreply.github.com>
2021-12-16 12:41:10 +0100

    Add files via upload
    new Program

---
+++ Fur Nico/NumberGuesser2/NumberGuess/DownloadTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  DownloadTableViewController.h
+//  NumberGuess
+//
+//  Created by Simon Ruspekhofer on 16.12.21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DownloadTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
